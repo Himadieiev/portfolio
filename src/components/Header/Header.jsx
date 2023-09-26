5a2036ce204f467de38529a3b4311550a6809284
Ruslan Himadieiev <113462988+Himadieiev@users.noreply.github.com>
2023-09-26 13:02:29 +0300

    added download CV

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import css from "./Header.module.css";
+import cv from "../../Ruslan_Himadieiev_Junior_Front-End_Developer.pdf";
 
 const Header = () => {
   return (
@@ -15,13 +16,8 @@ const Header = () => {
         <div className={css.header__text}>
           <p>with passion for learning and creating.</p>
         </div>
-        <a
-          href="https://www.linkedin.com/in/ruslan-himadieiev-32416b271/overlay/1635541764899/single-media-viewer/?profileId=ACoAAEJozgwB3ciXmG-8bIdBKmB5nD8S19DuB48"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={css.btn}
-        >
-          CV Link
+        <a href={cv} download title="download_CV" className={css.btn}>
+          Download CV
         </a>
       </div>
     </header>
