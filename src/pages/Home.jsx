c9be051bdff8f131eb76da60666b285f1cbeb879
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-05-24 15:45:09 +0300

    Update Home page text.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,13 +13,13 @@ const Home = () => {
             <li className="content-list__item">
               <h2 className="title-2">Frontend Skills</h2>
               <p>
-                HTML, CSS, SCSS, JavaScript, React, Redux, TypeScript, Angular, Rxjs, REST API, NPM,
-                GIT, GitHub, GitLab
+                HTML, CSS, SCSS, TW, JavaScript, TypeScript, React, Angular, Rxjs, Redux, Zustand,
+                REST API, NPM, GIT, GitHub, GitLab
               </p>
             </li>
             <li className="content-list__item">
               <h2 className="title-2">Backend Skills</h2>
-              <p>Node.js, Express, REST API, MongoDB, Mongoose</p>
+              <p>Node.js, Express, REST API, MongoDB, Mongoose, Appwrite, Supabase</p>
             </li>
             <li className="content-list__item">
               <h2 className="title-2">Hobbies</h2>
